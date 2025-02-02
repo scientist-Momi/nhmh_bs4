67e1ee9329b975e9b8622b6c43c0413a4dcc2ed0
scientist-Momi <124228573+scientist-Momi@users.noreply.github.com>
2025-02-02 21:23:43 +0000

    Update db_connector.php

--- includes/db_connector.php
+++ includes/db_connector.php
@@ -4,11 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "nhmh_db";
 
-// $hostname = "momiwebs.com.ng";
-// $username = "momiwebscom_nhmaternity";
-// $password = "GbPD)(xN[FGZ";
-// $dbname = "momiwebscom_nhmh_db";
-
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 
 if (!$conn) {
